526ac113feb7aadf24652a2ade4cf287cef919dc
Sandushke <sandushkedealwis9900@gmail.com>
2024-04-02 11:03:16 +0530

    Code updates

--- motionSensor.py
+++ motionSensor.py
@@ -1,19 +1,30 @@
-import RPi.GPIO as GPIO                       
-import time                                   
+import cv2
+import time
+from gpiozero import MotionSensor
 
-GPIO.setmode(GPIO.BOARD)                      
-pir = 26                                     
+# Initialize the motion sensor (connect it to GPIO pin 4)
+pir = MotionSensor(4)
 
-GPIO.setup(pir, GPIO.IN)                      
+# Initialize the webcam
+camera = cv2.VideoCapture(0)
 
-print ("Waiting for sensor to settle")
-
-time.sleep(2)                   
-print ("Detecting motion")
-
+# Main loop to detect motion and activate the camera
 while True:
-    if GPIO.input(pir):             
-        print ("Motion Detected!")
-        time.sleep(2)             
+    if pir.motion_detected:
+        print("Motion detected!")
+       
+        # Capture frame from the webcam
+        ret, frame = camera.read()
 
-time.sleep(0.1)  
+        # Generate a unique file name based on current timestamp
+        file_name = '/home/admin/sensor' + time.strftime("%Y%m%d-%H%M%S") + '.png'
+
+        # Save the captured frame as an image file
+        cv2.imwrite(file_name, frame)
+
+       
+    # Add a short delay to avoid continuously checking for motion
+    time.sleep(0.5)
+
+# Release the webcam
+camera.release()
