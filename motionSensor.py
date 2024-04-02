463361cdad7a8478754ef94493752f120ed9355d
Sandushke <sandushkedealwis9900@gmail.com>
2024-04-02 11:01:06 +0530

    Code updates

---
+++ motionSensor.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO                       
+import time                                   
+
+GPIO.setmode(GPIO.BOARD)                      
+pir = 26                                     
+
+GPIO.setup(pir, GPIO.IN)                      
+
+print ("Waiting for sensor to settle")
+
+time.sleep(2)                   
+print ("Detecting motion")
+
+while True:
+    if GPIO.input(pir):             
+        print ("Motion Detected!")
+        time.sleep(2)             
+
+time.sleep(0.1)  
