9b41b19c8b3812a30c74348b786ba0516770e994
Nadun Senarathne <nadunsenarathne18@gmail.com>
2024-04-05 15:23:33 +0530

    changes

--- capture_video_and_predict_cloud.py
+++ capture_video_and_predict_cloud.py
@@ -60,10 +60,10 @@ def predict_person_from_samples(frames):
 # video_path = f"gs://model1_bucket/captured_video.mp4"
 # target_video = f"/home/samanerendra"
 # os.system(f"gsutil cp {video_path} {target_video}")
-video = "captured_video.mp4"
+video_path = "captured_video.mp4"
 
 # Process the video and predict person
-sampled_frames = get_frames_from_video(video, 5)  # Use 5 frames from the video
+sampled_frames = get_frames_from_video(video_path, 5)  # Use 5 frames from the video
 person = predict_person_from_samples(sampled_frames)
 
 # Print the predicted person
