73683e00d4a062de1ecddad223b574eda315d327
Nadun Senarathne <nadunsenarathne18@gmail.com>
2024-04-07 13:34:41 +0530

    .

--- capture_video_and_predict_cloud.py
+++ capture_video_and_predict_cloud.py
@@ -11,14 +11,14 @@ from datetime import datetime
 import pytz
 
 
-# Define the path to your repository's root directory
-repo_path = '/home/samanerendra/Edge-AI-CW'
+# # Define the path to your repository's root directory
+# repo_path = '/home/samanerendra/Edge-AI-CW'
 
-# Ensure you're in the correct directory
-os.chdir(repo_path)
+# # Ensure you're in the correct directory
+# os.chdir(repo_path)
 
-# Pull the latest changes from the repository
-subprocess.run(['/usr/bin/git', 'pull'], check=True)
+# # Pull the latest changes from the repository
+# subprocess.run(['/usr/bin/git', 'pull'], check=True)
 
 # Start timing the entire script execution
 script_start_time = time.time()
@@ -115,6 +115,8 @@ video_path = "video_clip/captured_video.mp4"
 # Process the video and predict person
 sampled_frames = get_frames_from_video(video_path, 5)
 person = predict_person_from_samples(sampled_frames)
+
+print(f"Predicted person: {person}")
 
 # End timing the entire script execution
 script_end_time = time.time()
