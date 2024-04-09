31d04fae2e180239bf322f02effaf07b196de66d
Sandushke De Alwis <78974539+Sandushke@users.noreply.github.com>
2024-04-09 15:13:35 +0530

    Added code to capture video

--- sensor.py
+++ sensor.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 import time
-import cv2
+import subprocess
+import os
 
 # Set up GPIO mode and pins
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 TRIG_PIN = 18
 ECHO_PIN = 16
 GPIO.setup(TRIG_PIN, GPIO.OUT)
@@ -14,9 +15,10 @@ def measure_distance():
     GPIO.output(TRIG_PIN, True)
     time.sleep(0.00001)
     GPIO.output(TRIG_PIN, False)
-    
+
     while GPIO.input(ECHO_PIN) == 0:
         pulse_start = time.time()
+
     while GPIO.input(ECHO_PIN) == 1:
         pulse_end = time.time()
 
@@ -24,38 +26,39 @@ def measure_distance():
     distance = (pulse_duration * 34000) / 2
     return distance / 100  
 
-# Define function to activate webcam
-def activate_webcam():
-    print("Person Detected. Activating webcam!!!")
-    # Initialize webcam
-    camera = cv2.VideoCapture(0)
-    start_time = time.time()
-    
-    # Capture video for 5 seconds
-    while time.time() - start_time <= 5:
-        ret, frame = camera.read()
-        cv2.imshow('Webcam', frame)
-        cv2.waitKey(1)
-    
-    # Release webcam
-    camera.release()
-    cv2.destroyAllWindows()
-    print("Webcam deactivated.")
-
 try:
     while True:
         # Measure distance in meters
         dist = measure_distance()
-        
+
         # Check if distance is less than or equal to 1 meter
-        if dist <= 1:
+        if 0.1 < dist <= 1:
             print("Distance:", dist, "m")
-            activate_webcam()  # Activate webcam
+            # Execute the shell commands to capture video and push to GitHub
+
+            subprocess.run([
+                "ffmpeg", "-f", "v4l2", "-video_size", "1280x720", "-i", "/dev/video0", "-t", "2",
+                "/home/admin/videos/captured_video.mp4"
+            ])
+
+            subprocess.run(["mv", "/home/admin/videos/captured_video.mp4", "/home/admin/Edge-AI-CW/video_clip"])
+            os.chdir("/home/admin/Edge-AI-CW/video_clip")
+            subprocess.run(["git", "add", "captured_video.mp4"])
+            subprocess.run(["git", "commit", "-m", "Added a new video file to test the automate process without authentication"])
+            subprocess.run(["git", "config", "credential.helper", "store"])
+            subprocess.run(["git", "config", "--global", "user.name", "Osh0721"])
+            subprocess.run(["git", "config", "--global", "user.email", "oshanrathnayaka53@gmail.com"])
+            subprocess.run(["echo", "https://Osh0721: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@github.com", ">", "~/.git-credentials"])
+            subprocess.run(["git", "push", "origin", "main"])
+
+            print("Video captured and pushed to GitHub")
+            time.sleep(30)
+
         else:
             print("Distance:", dist, "m")
             print("Please move bit towrds the camera")
-            time.sleep(4)  # Wait for 1 second before checking again
-        
+            time.sleep(4)  # Wait for 4 seconds before checking again
+
 except KeyboardInterrupt:
     # Clean up GPIO
     GPIO.cleanup()
