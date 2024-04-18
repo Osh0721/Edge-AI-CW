17032c2f0e6e2dc91ee73da10fbd2eb1f92bb536
Sandushke De Alwis <78974539+Sandushke@users.noreply.github.com>
2024-04-18 08:58:25 +0530

    Code to indicate light signal

---
+++ traffic_light.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import psutil
+
+GREEN = 26
+YELLOW = 13
+RED = 19
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM)  
+GPIO.setwarnings(False)
+GPIO.setup(GREEN, GPIO.OUT)
+GPIO.setup(YELLOW, GPIO.OUT)
+GPIO.setup(RED, GPIO.OUT)
+
+try:
+   while (1):
+      cpu_pc = psutil.cpu_percent(interval=2)
+      print ('CPU: %d%%') % (cpu_pc)
+      if (cpu_pc ==90) :
+         GPIO.output(GREEN, False)
+         GPIO.output(YELLOW, False)
+         GPIO.output(RED, True)
+except KeyboardInterrupt:
+    print ("Good bye")
+    GPIO.output(GREEN, False)
+    GPIO.output(YELLOW, False)
+    GPIO.output(RED, False)
