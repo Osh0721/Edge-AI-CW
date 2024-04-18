f00a1d3a3886f1bb8470b4676a242f29aae75ce7
Sandushke <sandushkedealwis9900@gmail.com>
2024-04-18 21:34:08 +0530

    Code refactor: (to detect light in LED bulb)

--- traffic_light.py
+++ traffic_light.py
@@ -1,18 +1,36 @@
-import time
 import RPi.GPIO as GPIO
-
-class Led:
-	def __init__(self, pinNumber, name, duration):
-		self.pinNumber = pinNumber
-		self.name = name
-		self.duration = duration
-		GPIO.setup(pinNumber,GPIO.OUT)
-	
-	def on (self):
-		print(self.name + " on")
-		GPIO.output(self.pinNumber, GPIO.HIGH)
-
-	def off (self):
-		time.sleep(self.duration)
-		print(self.name + " off")
-		GPIO.output(self.pinNumber, GPIO.LOW)
+import time
+  
+try:
+  def lightTraffic(led1, led2, led3, delay ):
+    GPIO.output(led1, 1)
+    time.sleep(delay)
+    GPIO.output(led1, 0)
+    GPIO.output(led2, 1)
+    time.sleep(delay)
+    GPIO.output(led2, 0)
+    GPIO.output(led3, 1)  
+    time.sleep(delay)
+    GPIO.output(led3, 0)  
+  GPIO.setmode(GPIO.BCM)
+  button = 19
+  GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  ledGreen = 16
+  ledYellow = 12
+  ledRed = 23
+  GPIO.setup(ledGreen, GPIO.OUT)
+  GPIO.setup(ledYellow, GPIO.OUT)
+  GPIO.setup(ledRed, GPIO.OUT)
+  while True:
+    input_state = GPIO.input(button)
+    if input_state == False:
+      print('Button Pressed')
+      lightTraffic(ledGreen, ledYellow, ledRed, 1)
+    else: 
+      GPIO.output(ledGreen, 0)
+      GPIO.output(ledYellow, 0)
+      GPIO.output(ledRed, 0)
+except KeyboardInterrupt:
+  print ("You've exited the program")
+finally:
+  GPIO.cleanup()
