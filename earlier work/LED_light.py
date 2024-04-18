0e86258d2513f25ad747942f478636c1f867f0cd
Sandushke <sandushkedealwis9900@gmail.com>
2024-04-18 11:07:11 +0530

    Updated code to light up LED

--- earlier work/LED_light.py
+++ earlier work/LED_light.py
@@ -1,28 +1,36 @@
-# Import time, GPIO, and new Led class
-import time
 import RPi.GPIO as GPIO
-from led import Led
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# Create variables to hold the 3 LEDs
-redled = Led(18, "RED", 5)
-yellowled = Led(8, "YELLOW", 2) 
-greenled = Led(20, "GREEN", 5)
-# print("")
-
-#start traffic sequence
-redled.on()
-redled.off()
-
-greenled.on()
-greenled.off()
-
-yellowled.on()
-yellowled.off()
-
-redled.on()
-redled.off()
-
-GPIO.cleanup()
+import time
+  
+try:
+  def lightTraffic(led1, led2, led3, delay ):
+    GPIO.output(led1, 1)
+    time.sleep(delay)
+    GPIO.output(led1, 0)
+    GPIO.output(led2, 1)
+    time.sleep(delay)
+    GPIO.output(led2, 0)
+    GPIO.output(led3, 1)  
+    time.sleep(delay)
+    GPIO.output(led3, 0)  
+  GPIO.setmode(GPIO.BCM)
+  button = 19
+  GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  ledGreen = 16
+  ledYellow = 12
+  ledRed = 23
+  GPIO.setup(ledGreen, GPIO.OUT)
+  GPIO.setup(ledYellow, GPIO.OUT)
+  GPIO.setup(ledRed, GPIO.OUT)
+  while True:
+    input_state = GPIO.input(button)
+    if input_state == False:
+      print('Button Pressed')
+      lightTraffic(ledGreen, ledYellow, ledRed, 1)
+    else: 
+      GPIO.output(ledGreen, 0)
+      GPIO.output(ledYellow, 0)
+      GPIO.output(ledRed, 0)
+except KeyboardInterrupt:
+  print ("You've exited the program")
+finally:
+  GPIO.cleanup()
